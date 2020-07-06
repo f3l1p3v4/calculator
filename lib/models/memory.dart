2c08d93be7ffe64cb42cedfa7871e6aa52039894
f3l1p3v4 <fvaldez018@gmail.com>
2020-07-06 18:08:14 -0400

    added calculate

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -3,7 +3,7 @@ class Memory {
 
   final _buffer = [0.0, 0.0];
   int _bufferIndex = 0;
-  String operation;
+  String _operation;
   String _value = '0';
   bool _wipeValue = false;
 
@@ -19,6 +19,20 @@ class Memory {
   }
 
   _setOperation(String newOperation) {
+    if(_bufferIndex == 0) {
+      _operation = newOperation;
+      _bufferIndex = 1;
+    } else {
+      _buffer[0] = _calculate();
+      _buffer[1] = 0.0;
+      _value = _buffer[0].toString();
+      _value = _value.endsWith('.0') ? _value.split('.')[0] : _value;
+    
+      bool isEqualSign = newOperation == '=';
+      _operation = isEqualSign ? null : newOperation;
+      _bufferIndex = isEqualSign ? 0 : 1;
+    }
+
     _wipeValue = true;
   }
 
@@ -40,6 +54,21 @@ class Memory {
 
   _allClear() {
     _value = '0';
+    _buffer.setAll(0, [0.0, 0.0]);
+    _operation = null;
+    _bufferIndex = 0;
+    _wipeValue = false;
+  }
+
+  _calculate() {
+    switch(_operation) {
+      case '%': return _buffer[0] % _buffer[1];
+      case '/': return _buffer[0] / _buffer[1];
+      case 'x': return _buffer[0] * _buffer[1];
+      case '-': return _buffer[0] - _buffer[1];
+      case '+': return _buffer[0] + _buffer[1];
+      default: return _buffer[0];
+    }
   }
 
   String get value {
